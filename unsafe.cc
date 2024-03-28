8930cc42163f8dff09e535d5e173faf0e8ae4d40
Essig <73817645+RaydanOMGr@users.noreply.github.com>
2024-03-28 18:36:06 +0100

    Update unsafe.cc

--- unsafe.cc
+++ unsafe.cc
@@ -65,6 +65,29 @@ namespace {
     args.GetReturnValue().Set(v8::BigInt::New(isolate, address));
   }
 
+  // Function to set the value at a memory location
+  void Set(const v8::FunctionCallbackInfo<v8::Value>& args) {
+    v8::Isolate* isolate = args.GetIsolate();
+    if (args.Length() < 2) {
+      isolate->ThrowException(v8::Exception::TypeError(
+          v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
+      return;
+    }
+
+    if (!args[0]->IsBigInt()) {
+      isolate->ThrowException(v8::Exception::TypeError(
+          v8::String::NewFromUtf8(isolate, "First argument must be a bigint").ToLocalChecked()));
+      return;
+    }
+
+    v8::Local<v8::Object> obj = args[1]->ToObject(isolate->GetCurrentContext()).ToLocalChecked();
+
+    void* ptr = reinterpret_cast<void*>(args[0].As<v8::BigInt>()->Uint64Value());
+
+    // Set the value at the memory location
+    *(reinterpret_cast<v8::Local<v8::Value>*>(ptr)) = obj;
+  }
+
   // Function to get the object at a memory location
   void Get(const v8::FunctionCallbackInfo<v8::Value>& args) {
     v8::Isolate* isolate = args.GetIsolate();
