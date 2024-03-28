4f6eec00afae1c1e358f438518bfe917555c2fb5
Essig <73817645+RaydanOMGr@users.noreply.github.com>
2024-03-28 17:41:55 +0100

    Update unsafe.cc

--- unsafe.cc
+++ unsafe.cc
@@ -7,23 +7,23 @@ namespace {
     v8::Isolate* isolate = args.GetIsolate();
     if (args.Length() < 1) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Wrong number of arguments")));
+          v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
       return;
     }
 
     if (!args[0]->IsNumber()) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Argument must be a number")));
+          v8::String::NewFromUtf8(isolate, "Argument must be a number").ToLocalChecked()));
       return;
     }
 
-    size_t amount = args[0]->NumberValue();
+    size_t amount = args[0]->IntegerValue(isolate->GetCurrentContext()).ToChecked();
 
     // Allocate memory
     void* ptr = malloc(amount);
 
     // Return the memory address
-    args.GetReturnValue().Set(v8::BigInt::NewFromUnsigned(isolate, reinterpret_cast<uintptr_t>(ptr)));
+    args.GetReturnValue().Set(v8::BigInt::New(isolate, reinterpret_cast<uintptr_t>(ptr)));
   }
 
   // Function to free memory
@@ -31,13 +31,13 @@ namespace {
     v8::Isolate* isolate = args.GetIsolate();
     if (args.Length() < 1) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Wrong number of arguments")));
+          v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
       return;
     }
 
     if (!args[0]->IsBigInt()) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Argument must be a bigint")));
+          v8::String::NewFromUtf8(isolate, "Argument must be a bigint").ToLocalChecked()));
       return;
     }
 
@@ -52,7 +52,7 @@ namespace {
     v8::Isolate* isolate = args.GetIsolate();
     if (args.Length() < 1) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Wrong number of arguments")));
+          v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
       return;
     }
 
@@ -62,7 +62,7 @@ namespace {
     uintptr_t address = reinterpret_cast<uintptr_t>(*obj);
 
     // Return the memory address
-    args.GetReturnValue().Set(v8::BigInt::NewFromUnsigned(isolate, address));
+    args.GetReturnValue().Set(v8::BigInt::New(isolate, address));
   }
 
   // Function to set the value at a memory location
@@ -70,13 +70,13 @@ namespace {
     v8::Isolate* isolate = args.GetIsolate();
     if (args.Length() < 2) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Wrong number of arguments")));
+          v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
       return;
     }
 
     if (!args[0]->IsBigInt()) {
       isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "First argument must be a bigint")));
+          v8::String::NewFromUtf8(isolate, "First argument must be a bigint").ToLocalChecked()));
       return;
     }
 
